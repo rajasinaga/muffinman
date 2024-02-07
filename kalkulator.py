74257badaf547876e4f26ab1fac9ed322702c8df
rajasinaga <93964373+rajasinaga@users.noreply.github.com>
2024-02-07 14:44:15 +0700

    Update kalkulator.py
    added unique key in functions st.number_input

--- kalkulator.py
+++ kalkulator.py
@@ -6,13 +6,13 @@ st.subheader('why are you here?')
 c1, c2, c3, c4 = st.columns(4)
 
 with c1:
-  a = st.number_input('Real Integers', value=0)
+  a = st.number_input('Real Integers', value=0, key= 'a')
 
 with c2:
   Operators = st.selectbox('Operators',('+', '-', 'x', ':'), key='k1')
 
 with c3:
-  b = st.number_input('Real Integers', value=0)
+  b = st.number_input('Real Integers', value=0, key= 'b')
 
 with c4:
   if Operators == '+':
